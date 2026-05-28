ca4e83c97727ed6498d5632c5fb3c982b016db94
Carlos Maya <estebanmaya163@gmail.com>
2026-05-28 17:01:47 -0500

    Primera Subida

---
+++ Cap1.Fundamentos/ex02/ex02.js
@@ -0,0 +1,28 @@
+var nombre; //Declaracion de la variable ´nombre´
+nombre = "Juan"; // Asignacion de un valor a la varaiable ´nombre´
+console.log(nombre); // Muestra el valor de la variable ´nombre´ en la consola
+
+var nombre="Maria"; // Reasignacion de un nuevo valor a la variable ´nombre´
+console.log(nombre); // Muestra el nuevo valor de la variable ´nombre´ en la consola
+var nombre="Carlos"; // Reasignacion de un nuevo valor a la variable ´nombre´
+console.log(nombre); // Muestra el nuevo valor de la variable ´nombre´ en la consola
+var nombre="Ana"; // Reasignacion de un nuevo valor a la variable ´nombre´
+console.log(nombre); // Muestra el nuevo valor de la variable ´nombre´ en la consola
+
+/*
+    Uso del la palabra reservada 'let' para declarar una variable llamada 'bebida' y asignarle diferentes valores a lo largo del código. Cada vez que se asigna un nuevo valor a la variable 'bebida', se muestra su contenido en la consola.
+
+    Diferencia entre var y let: La palabra reservada 'var' tiene un alcance de función, lo que significa que la variable declarada con 'var' es accesible dentro de la función en la que se declara. Por otro lado, 'let' tiene un alcance de bloque, lo que significa que la variable declarada con 'let' solo es accesible dentro del bloque en el que se declara (por ejemplo, dentro de un bucle o una estructura condicional). Además, 'let' no permite redeclarar la misma variable dentro del mismo bloque, mientras que 'var' sí lo permite.
+*/
+
+let bebida; // Declaracion de la varibale ´bebida´
+bebida = "Coca-Cola"; // Asignacion de un valor a la variable ´bebida´
+console.log(bebida); //Muestra el valor de la variable ´bebida´ en la consola
+
+bebida = "Sprite"; // Asignacion de un valor a la variable ´bebida´
+console.log(bebida); //Muestra el valor de la variable ´bebida´ en la consola
+
+bebida = "Fanta"; // Asignacion de un valor a la variable ´bebida´
+console.log(bebida); //Muestra el valor de la variable ´bebida´ en la consola
+
+// uso constante para declarar una variable constante llamada PI 
