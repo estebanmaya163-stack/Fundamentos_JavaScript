d9d9d6f4150f0183bdbdc3c0ebad8a26bbd70a53
Your Name <you@example.com>
2026-06-11 16:35:44 -0500

    JavaScript

---
+++ JAVASCRIPT/cap01.js
@@ -0,0 +1,47 @@
+/*
+===================================================================
+INTRODUCCION A JAVASCRIPT
+===================================================================
+
+JavaScript es un lenguaje de programacion
+que permite agregar comportamiento a las paginas web.
+
+Todo el codigo de este archivo sera ejecutado
+por el navegador de arriba hacia abajo.
+*/
+
+console.log("Hola Mundo");
+
+/*
+===========================================================
+MULTIPLE MENSAJES
+===========================================================
+
+console.log() permite mostrar informacion 
+en la consola del navegador.
+*/
+
+console.log("Mensaje 1")
+console.log("Mensaje 2")
+console.log("Mensaje 3")
+console.log("Mensaje 4")
+
+/*
+===============================================================
+COMENTARIOS
+=============================================================
+
+Los comentarios sirven para documentar el codigo.
+
+El navegador los ignora.
+*/
+
+// Comentario de una linea
+
+/*
+comentario
+de multiples
+lineas
+*/
+
+console.log("Los comentarios no aparecen")
